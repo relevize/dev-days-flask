e2cef85a32c884cadb05699838fad61628ba5aeb
sheryak_n <nathanraysheryak@gmail.com>
2022-06-27 15:44:59 -0400

    make 'rank' enum, enforce at db and schema

--- captains_log/models/CrewMember.py
+++ captains_log/models/CrewMember.py
@@ -1,11 +1,20 @@
+import enum
 from marshmallow import Schema, fields
+from marshmallow_enum import EnumField
 
 from ..extensions import db
+
+class RankEnum(enum.Enum):
+    ensign = 0
+    lieutenant = 1
+    lieutenant_commander = 2 
+    commander = 3
+    captain = 4
 
 class CrewMember(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    rank = db.Column(db.String(80), nullable=False) # turn this into enum
+    rank = db.Column(db.Enum(RankEnum), nullable=False)
 
     def __repr__(self):
         return '<CrewMember %r>' % self.name
@@ -14,7 +23,7 @@ class CrewMember(db.Model):
 class CrewMemberSchema(Schema):
     id = fields.Int()
     name = fields.Str()
-    rank = fields.Str()
+    rank = EnumField(RankEnum)
 
 crew_member_schema = CrewMemberSchema()
 crew_members_schema = CrewMemberSchema(many=True)
