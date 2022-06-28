f1046399b8921aea8f33675d5ccf6f42f0939d50
sheryak_n <nathanraysheryak@gmail.com>
2022-06-28 09:20:04 -0400

    add redacted to log + migration

--- captains_log/models/Log.py
+++ captains_log/models/Log.py
@@ -5,8 +5,9 @@ from ..extensions import db
 
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    star_date = db.Column(db.DateTime, default=datetime.utcnow)
+    star_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     log_entry = db.Column(db.Text, nullable=False)
+    redacted = db.Column(db.Boolean, nullable=False, default=False)
 
     crew_member_id = db.Column(db.Integer, db.ForeignKey('crew_member.id'), nullable=False)
     crew_member = db.relationship('CrewMember', backref=db.backref('logs', lazy=True))
@@ -18,6 +19,7 @@ class LogSchema(Schema):
     id = fields.Int()
     star_date = fields.DateTime()
     log_entry = fields.Str()
+    redacted = fields.Bool()
     crew_member_id = fields.Int()
 
 log_schema = LogSchema()
